cfb4d683013da13339af1ab07dfd8c1e1efda13a
gulshan0201 <84889171+gulshan0201@users.noreply.github.com>
2021-12-24 12:18:27 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,17 @@
+//Assignment Dec22...
+
+var arr = [];
+
+var i = prompt("Enter first number");
+i = parseInt(i);
+arr.push(i);
+
+i = prompt("Enter second number");
+i = parseInt(i);
+arr.push(i);
+
+i = prompt("Enter third number");
+i = parseInt(i);
+arr.push(i);
+
+console.log(arr);
